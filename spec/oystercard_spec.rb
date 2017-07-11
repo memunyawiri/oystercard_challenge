41730edd19edad43a5229b85d253f710bb93ac19
Mutsa <mutsa.evelyn@hotmail.com>
2017-07-11 12:33:24 +0100

    rafctored top_up test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,12 +1,14 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'has a balance of zero' do
-    expect(subject.balance).to eq(0)
+  subject(:card) { described_class.new }
+
+  describe 'Oystercard has intial balance' do
+    it 'has a balance of zero' do
+      expect(subject.balance).to eq(0)
+    end
   end
   describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
-
     it 'can top up the balance' do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
