6448a29ab3e8b371922f2ac3ce3d43aa89e8f2d2
Mutsa <mutsa.evelyn@hotmail.com>
2017-07-12 12:35:53 +0100

    feature spec file updated

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,15 +1,15 @@
 require './lib/oystercard'
 
 oystercard = Oystercard.new
-station = Station.new
+#station = Station.new
 oystercard.top_up(4)
-oystercard.touch_in(station)
+#oystercard.touch_in(station)
 p oystercard
 oystercard.balance
 p oystercard
 oystercard.top_up(20)
 p oystercard
-oystercard.touch_in
+#oystercard.touch_in
 p oystercard
 oystercard.touch_out
 p oystercard
